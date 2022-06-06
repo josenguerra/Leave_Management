a10b88bc9fcf2fb35c21aeaf65f1260f182766fe
josenguerra <josenguerra@hotmail.com>
2022-06-06 16:17:09 +0100

    Add EF Core Contexts

---
+++ LeaveManagement/src/Infrastructure/LeaveManagement.Persistence/HrLeaveManagementDbContext.cs
@@ -0,0 +1,24 @@
+﻿using LeaveManagement.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeaveManagement.Persistence
+{
+    public class HrLeaveManagementDbContext : AuditableDbContext
+    {
+        public HrLeaveManagementDbContext(DbContextOptions<HrLeaveManagementDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(HrLeaveManagementDbContext).Assembly);
+        }
+
+
+        public DbSet<LeaveRequest> LeaveRequests { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+
+    }
+}
