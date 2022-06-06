70e205a3b655be56a308d6d0c0310af502392745
josenguerra <josenguerra@hotmail.com>
2022-06-06 15:28:09 +0100

    Add Leaves Validators

---
+++ LeaveManagement/src/Core/LeaveManagement.Application/DTOs/LeaveRequest/Validators/CreateLeaveRequestDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using LeaveManagement.Application.Contracts.Persistence;
+
+namespace LeaveManagement.Application.DTOs.LeaveRequest.Validators
+{
+    public class CreateLeaveRequestDtoValidator : AbstractValidator<CreateLeaveRequestDto>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+
+        public CreateLeaveRequestDtoValidator(ILeaveTypeRepository leaveTypeRepository)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            Include(new ILeaveRequestDtoValidator(_leaveTypeRepository));
+
+        }
+    }
+}
