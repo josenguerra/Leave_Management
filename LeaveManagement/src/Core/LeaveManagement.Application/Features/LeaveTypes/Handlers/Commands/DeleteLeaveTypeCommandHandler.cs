3e4e62633b13a261f4a2f0c91bfbf1259a9f9aa5
josenguerra <josenguerra@hotmail.com>
2022-06-06 15:17:54 +0100

    Delete Leaves Command and Handlers

---
+++ LeaveManagement/src/Core/LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/DeleteLeaveTypeCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using LeaveManagement.Application.Contracts.Persistence;
+using LeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using MediatR;
+
+namespace LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
+{
+    public class DeleteLeaveTypeCommandHandler : IRequestHandler<DeleteLeaveTypeCommand>
+    {
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper)
+        {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(DeleteLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.Get(request.Id);
+
+
+            await _leaveTypeRepository.Delete(leaveType);
+
+            return Unit.Value;
+        }
+    }
+}
