2f888006d9c95653bdeaf165716f1245b2aa8cc3
josenguerra <josenguerra@hotmail.com>
2022-06-06 16:28:27 +0100

    Add repositories implementation

---
+++ LeaveManagement/src/Core/LeaveManagement.Application/Constants/CustomClaimTypes.cs
@@ -0,0 +1,7 @@
+﻿namespace LeaveManagement.Application.Constants
+{
+    public static class CustomClaimTypes
+    {
+        public const string Uid = "uid";
+    }
+}
---
+++ LeaveManagement/src/Infrastructure/LeaveManagement.Persistence/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using LeaveManagement.Application.Contracts.Persistence;
+using LeaveManagement.Domain;
+
+namespace LeaveManagement.Persistence.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        private readonly HrLeaveManagementDbContext _dbContext;
+
+        public LeaveTypeRepository(HrLeaveManagementDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
