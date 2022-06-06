f704409caba97560a9668e3efb0a6d83d24916c4
josenguerra <josenguerra@hotmail.com>
2022-06-06 16:03:02 +0100

    Add missing Mapping DTOs

--- LeaveManagement/src/Core/LeaveManagement.Application/Profiles/MappingProfile.cs
+++ LeaveManagement/src/Core/LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -12,14 +12,22 @@ namespace LeaveManagement.Application.Profiles
         {
             #region LeaveRequest Mappings
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestListDto>()
+                .ForMember(dest => dest.DateRequested, opt => opt.MapFrom(src => src.DateCreated))
+                .ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
             #endregion LeaveRequest
 
-            #region Leave Allocation Mappings
+            #region Leave Allocation
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
             #endregion
 
-            #region Leave Type Mappings
-            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap(); 
+            #region Leave Type
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap(); 
             #endregion
         }
     }
