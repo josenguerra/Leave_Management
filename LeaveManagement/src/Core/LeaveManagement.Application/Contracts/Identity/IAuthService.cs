608af3d76e69f64568b3f026a4e42ca3e05091f2
josenguerra <josenguerra@hotmail.com>
2022-06-14 16:03:00 +0100

    Add identity project. Add account requests to API

---
+++ LeaveManagement/src/Core/LeaveManagement.Application/Contracts/Identity/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using LeaveManagement.Application.Models.Identity;
+
+namespace LeaveManagement.Application.Contracts.Identity
+{
+    public interface IAuthService
+    {
+        Task<AuthResponse> Login(AuthRequest request);
+        Task<RegistrationResponse> Register(RegistrationRequest request);
+    }
+}
